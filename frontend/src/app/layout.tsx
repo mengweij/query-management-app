54eef4ccc0e209b6297576b1ed88cf15fd5c8448
mengwei <mengwei@example.com>
2025-05-01 15:28:10 -0700

    feat: create frontend template and install dependencies

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,27 @@
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
+
+import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
+
+export const metadata = {
+  title: 'Query Management App',
+  description: 'A simple query management app to support EDC system',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}
