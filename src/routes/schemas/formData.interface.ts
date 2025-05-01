e27bfdfdb01e4b45414c6fbd72b6911e2f9884a6
mengwei <mengwei@example.com>
2025-05-01 10:49:18 -0700

    fix: update IFormData

--- src/routes/schemas/formData.interface.ts
+++ src/routes/schemas/formData.interface.ts
@@ -1,7 +1,10 @@
+import { Query } from '@prisma/client'
+
 export interface IFormData {
   id: string
   question: string
   answer: string
+  query: Query | null
 }
 
 export interface ICountedFormData {
