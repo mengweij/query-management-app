bbcb8119b6879fd39e5292a58079442cbf91f5af
mengwei <mengwei@example.com>
2025-05-01 17:50:06 -0700

    feat: add api services for frontend

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const server: FastifyInstance = build({
 
 server.register(CORSConfig, {
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 })
 
 server
